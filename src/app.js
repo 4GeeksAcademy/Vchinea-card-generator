77b20104267026c4be7d2514d618598d2b0e6380
Victor Chinea <77308527+nobystanders@users.noreply.github.com>
2024-07-15 22:40:38 +0000

    done

--- src/app.js
+++ src/app.js
@@ -9,3 +9,48 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const suits = ["♥", "♠", "♣", "♦"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+function getRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  return { suit: randomSuit, value: randomValue };
+}
+
+function displayRandomCard() {
+  const card = getRandomCard();
+
+  const cardSuitTop = document.getElementById("cardSuitTop");
+  const cardValue = document.getElementById("cardValue");
+  const cardSuitBottom = document.getElementById("cardSuitBottom");
+
+  cardSuitTop.textContent = card.suit;
+  cardValue.textContent = card.value;
+  cardSuitBottom.textContent = card.suit;
+
+  if (card.suit === "♥" || card.suit === "♦") {
+    cardSuitTop.style.color = "red";
+    cardSuitBottom.style.color = "red";
+  } else {
+    cardSuitTop.style.color = "black";
+    cardSuitBottom.style.color = "black";
+  }
+}
+
+window.onload = displayRandomCard;
